c07019433d164461db6eaaa56fc65980b56019b5
tmazitov <timurmazitov000@gmail.com>
2025-06-11 11:55:23 +0400

    update misspeling in the playerJoin handler

--- src/api/ws/clientSideHandlers/playerJoin.ts
+++ src/api/ws/clientSideHandlers/playerJoin.ts
@@ -6,7 +6,7 @@ import Logger from "../../../pkg/ws/logger";
 import WebSocketRequest from "../../../pkg/ws/request";
 import sendAuthorized from "../serverSideHandlers/authorized";
 import sendMatchOpponentConnected from "../serverSideHandlers/matchOpponentConnected";
-import sendUnautorized from "../serverSideHandlers/unauthorized";
+import sendUnauthorized from "../serverSideHandlers/unauthorized";
 import WS_CLIENT from "./handlers";
 
 export default async function playerMatchJoin(client:Client, request: WebSocketRequest<WS_CLIENT>) {
@@ -14,13 +14,13 @@ export default async function playerMatchJoin(client:Client, request: WebSocketR
     // Receive accessToken from payload
     const payload = request.payload as {accessToken: string};
     if (!payload || !payload.accessToken) {
-        return sendUnautorized(client);
+        return sendUnauthorized(client);
     }
     
     // Validate token and if it's valid get its payload
     const tokenPayload = await isTokenValid(payload.accessToken) as {userId: number}
     if (!tokenPayload || !tokenPayload.userId) {
-        return sendUnautorized(client);
+        return sendUnauthorized(client);
     }
 
 	client.setUserId(tokenPayload.userId);
@@ -28,7 +28,7 @@ export default async function playerMatchJoin(client:Client, request: WebSocketR
 	try {
 		Server.joinClient(client);
 	} catch(e) {
-		sendUnautorized(client);
+		sendUnauthorized(client);
 		return;
 	}
 
--- src/api/ws/serverSideHandlers/unauthorized.ts
+++ src/api/ws/serverSideHandlers/unauthorized.ts
@@ -1,6 +1,6 @@
 import Client from "../../../pkg/ws/client";
 import WS_SERVER from "./handlers";
 
-export default function sendUnautorized(client:Client) {
+export default function sendUnauthorized(client:Client) {
     client.send(WS_SERVER.Unauthorized);
 }   
